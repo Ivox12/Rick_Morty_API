2d80022bb21581477724bd317d145c5cc83467c7
Ivox12 <ivorochabf@gmail.com>
2023-01-19 18:00:19 -0300

    update

--- rick.js
+++ rick.js
@@ -2,6 +2,7 @@ let id = 1
 async function get_char (){
     let card = document.querySelector(".quadradao");
     let limit = 20
+
     for (i=1; i<=limit; i++){
         await fetch("https://rickandmortyapi.com/api/character/" + id)
         .then(Response => Response.json())
@@ -10,14 +11,19 @@ async function get_char (){
             console.log(id)
 
             card.innerHTML +=`
-            <div>
-                <h1>${get_char.id}</h1>
-                <h1>${get_char.name}</h1>
-                <img src="${get_char.image}">
-                <p>${get_char.status}</p>
-                <p>${get_char.gender}</p>
-                <p>${get_char.species}</p>
-                <p>${get_char.location.name}</P>
+            <div class="card">
+                    <img src="${get_char.image}">
+                <div class="name">
+                    <h2>${get_char.name}</h1>
+                </div>
+                <div class="${get_char.status}">
+                    <span><input type="radio"><span>
+                    <p>Status: ${get_char.status}</p>
+                </div>
+                <div class"details">
+                    <p>${get_char.species} <br> Gender: ${get_char.gender}</p><br>
+                    <p>Last Location: ${get_char.location.name}</p>
+                </div>
             </div>
             `
             id++
@@ -25,3 +31,5 @@ async function get_char (){
         limit=+20
     }
 }
+
+get_char()

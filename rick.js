aaa22bf57bf891d51ac44051dfc7503890b95593
Ivox12 <ivorochabf@gmail.com>
2023-01-19 21:28:17 -0300

    update

--- rick.js
+++ rick.js
@@ -1,7 +1,15 @@
 let id = 1
 async function get_char (){
     let card = document.querySelector(".quadradao");
-    let limit = 20
+    let btn = document.getElementById("more");
+    let portal = document.getElementById("portal");
+    let text = document.getElementById("click_here");
+    let limit = 20;
+
+    text.style.display="none"
+    portal.style.display="none"
+    document.body.style.backdropFilter= "blur(12px)";
+    btn.style.display="block"
 
     for (i=1; i<=limit; i++){
         await fetch("https://rickandmortyapi.com/api/character/" + id)
@@ -11,25 +19,26 @@ async function get_char (){
             console.log(id)
 
             card.innerHTML +=`
-            <div class="card">
-                    <img src="${get_char.image}">
-                <div class="name">
-                    <h2>${get_char.name}</h1>
+            <article>
+                <img src="${get_char.image}">
+                <div class="card">
+                    <div class="name">
+                        <h2>${get_char.name}</h1>
+                    </div>
+                    <div class="${get_char.status}">
+                        <span><input type="radio">
+                            ${get_char.status} - ${get_char.species}
+                        </span>
+                    </div>
+                    <div class="details">
+                        <span>Gender: ${get_char.gender}</span><br>
+                        <span>Last Location: ${get_char.location.name}</span>
+                    </div>
                 </div>
-                <div class="${get_char.status}">
-                    <span><input type="radio"><span>
-                    <p>Status: ${get_char.status}</p>
-                </div>
-                <div class"details">
-                    <p>${get_char.species} <br> Gender: ${get_char.gender}</p><br>
-                    <p>Last Location: ${get_char.location.name}</p>
-                </div>
-            </div>
+            </article>
             `
             id++
         })
         limit=+20
     }
 }
-
-get_char()
